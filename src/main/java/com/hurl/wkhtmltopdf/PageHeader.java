8ba052fa75a481fe5be49af34d48a588e9e5ba71
Ron Hu <hurongliang@gmail.com>
2014-07-28 17:30:52 +0800

    Add comments for PageHeader

--- src/main/java/com/hurl/wkhtmltopdf/PageHeader.java
+++ src/main/java/com/hurl/wkhtmltopdf/PageHeader.java
@@ -1,5 +1,10 @@
 package com.hurl.wkhtmltopdf;
 
+/**
+ * Page header to be added to each page
+ * @author Administrator
+ *
+ */
 public class PageHeader {
 	private String centerText;
 	private String leftText;
@@ -11,30 +16,52 @@ public class PageHeader {
 	public String getCenterText() {
 		return centerText;
 	}
+	
+	/**
+	 * Set text to be displayed at the center of header.
+	 * @param centerText
+	 */
 	public void setCenterText(String centerText) {
 		this.centerText = centerText;
 	}
 	public String getLeftText() {
 		return leftText;
 	}
+	/**
+	 * Set test to be displayed at the left of header.
+	 * @return
+	 */
 	public void setLeftText(String leftText) {
 		this.leftText = leftText;
 	}
 	public String getRightText() {
 		return rightText;
 	}
+
+	/**
+	 * Set test to be displayed at the right of header.
+	 * @return
+	 */
 	public void setRightText(String rightText) {
 		this.rightText = rightText;
 	}
 	public String getFontName() {
 		return fontName;
 	}
+	/**
+	 * Set font name of the header text.
+	 * @param fontName
+	 */
 	public void setFontName(String fontName) {
 		this.fontName = fontName;
 	}
 	public int getFontSize() {
 		return fontSize;
 	}
+	/**
+	 * Set font size of the header text.
+	 * @param fontSize
+	 */
 	public void setFontSize(int fontSize) {
 		this.fontSize = fontSize;
 	}
@@ -42,12 +69,20 @@ public class PageHeader {
 	public int getSpace() {
 		return space;
 	}
+	/**
+	 * Set space between header and content.
+	 * @param space
+	 */
 	public void setSpace(int space) {
 		this.space = space;
 	}
 	public boolean isDisplayLine() {
 		return displayLine;
 	}
+	/**
+	 * Set whether display a line under the header.
+	 * @param displayLine display line under header if true, or not display line under header if false.
+	 */
 	public void setDisplayLine(boolean displayLine) {
 		this.displayLine = displayLine;
 	}
